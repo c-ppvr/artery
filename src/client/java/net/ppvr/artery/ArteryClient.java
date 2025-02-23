701ded58b11e905dd3bf8a88813bc84622ea2a39
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-02-23 12:59:57 +0800

    feat: add ore generation

--- src/client/java/net/ppvr/artery/ArteryClient.java
+++ src/client/java/net/ppvr/artery/ArteryClient.java
@@ -1,11 +1,18 @@
 package net.ppvr.artery;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.gen.GenerationStep;
+import net.ppvr.artery.datagen.feature.ArteryOrePlacedFeatures;
 import net.ppvr.artery.gui.screen.ArteryScreens;
 
 public class ArteryClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ArteryScreens.initialize();
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ArteryOrePlacedFeatures.ORE_ERYTHIRTE);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ArteryOrePlacedFeatures.ORE_LEUKIUM);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ArteryOrePlacedFeatures.ORE_THROMBIUM);
     }
 }
--- src/client/java/net/ppvr/artery/datagen/ArteryDataGenerator.java
+++ src/client/java/net/ppvr/artery/datagen/ArteryDataGenerator.java
@@ -2,6 +2,10 @@ package net.ppvr.artery.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+import net.ppvr.artery.datagen.feature.ArteryOreConfiguredFeatures;
+import net.ppvr.artery.datagen.feature.ArteryOrePlacedFeatures;
 import net.ppvr.artery.datagen.recipe.ArteryRecipeProvider;
 
 public class ArteryDataGenerator implements DataGeneratorEntrypoint {
@@ -12,5 +16,13 @@ public class ArteryDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(ArteryBlockLootTableProvider::new);
         pack.addProvider(ArteryBlockTagProvider::new);
         pack.addProvider(ArteryModelProvider::new);
+        pack.addProvider(ArteryDynamicRegistriesProvider::new);
+    }
+
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+        registryBuilder
+                .addRegistry(RegistryKeys.CONFIGURED_FEATURE, ArteryOreConfiguredFeatures::bootstrap)
+                .addRegistry(RegistryKeys.PLACED_FEATURE, ArteryOrePlacedFeatures::bootstrap);
     }
 }
