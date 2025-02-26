035ff78e730d662e8c9bb1ca052d326f1ecb35af
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-02-26 09:27:47 +0800

    refactor: move map initializer

--- src/main/java/net/ppvr/artery/items/ArteryToolMaterial.java
+++ src/main/java/net/ppvr/artery/items/ArteryToolMaterial.java
@@ -9,10 +9,9 @@ import java.util.Map;
 public class ArteryToolMaterial {
     public static final ToolMaterial LEUKIUM = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 371, 6.0f, 2.0F, 16, ArteryItemTags.LEUKIUM_TOOL_MATERIALS);
 
-    public static final Map<Object, Double> TRANSFUSION_RATE_MAP;
+    public static final Map<Object, Double> TRANSFUSION_RATE_MAP = new HashMap<>();
 
     static {
-        TRANSFUSION_RATE_MAP = new HashMap<>();
         TRANSFUSION_RATE_MAP.put(LEUKIUM, -1.5);
     }
 }
