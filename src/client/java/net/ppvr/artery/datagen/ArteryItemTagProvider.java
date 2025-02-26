1c4808da4cace0d942a8dbfcbccf237f503e5349
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-02-26 23:28:28 +0800

    feat: add appropriate tags

--- src/client/java/net/ppvr/artery/datagen/ArteryItemTagProvider.java
+++ src/client/java/net/ppvr/artery/datagen/ArteryItemTagProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 import net.ppvr.artery.items.ArteryItemTags;
 import net.ppvr.artery.items.ArteryItems;
 
@@ -19,5 +20,13 @@ public class ArteryItemTagProvider extends FabricTagProvider<Item> {
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
         getOrCreateTagBuilder(ArteryItemTags.LEUKIUM_TOOL_MATERIALS).add(ArteryItems.LEUKIUM_INGOT);
         getOrCreateTagBuilder(ArteryItemTags.REPAIRS_THROMBIUM_ARMOR).add(ArteryItems.THROMBIUM_INGOT);
+
+        getOrCreateTagBuilder(ItemTags.SWORDS).add(ArteryItems.LEUKIUM_SWORD);
+        getOrCreateTagBuilder(ItemTags.AXES).add(ArteryItems.LEUKIUM_AXE);
+
+        getOrCreateTagBuilder(ItemTags.HEAD_ARMOR).add(ArteryItems.THROBIUM_HELMET);
+        getOrCreateTagBuilder(ItemTags.CHEST_ARMOR).add(ArteryItems.THROBIUM_CHESTPLATE);
+        getOrCreateTagBuilder(ItemTags.LEG_ARMOR).add(ArteryItems.THROBIUM_LEGGINGS);
+        getOrCreateTagBuilder(ItemTags.FOOT_ARMOR).add(ArteryItems.THROBIUM_BOOTS);
     }
 }
