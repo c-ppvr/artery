0488eb891582b1bb3dccd97937ff5bc841b04eec
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-03-01 15:12:20 +0800

    feat: implement quickMove method

--- src/main/java/net/ppvr/artery/screen/FibroblasterScreenHandler.java
+++ src/main/java/net/ppvr/artery/screen/FibroblasterScreenHandler.java
@@ -11,20 +11,58 @@ import net.minecraft.screen.slot.Slot;
 public class FibroblasterScreenHandler extends ScreenHandler {
     private final Inventory inventory;
     private final PropertyDelegate propertyDelegate;
-    private final Slot slot;
 
     public FibroblasterScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate propertyDelegate) {
         super(ArteryScreenHandlerTypes.FIBROBLASTER_SCREEN_HANDLER, syncId);
         this.inventory = inventory;
         this.propertyDelegate = propertyDelegate;
-        this.slot = addSlot(new Slot(inventory, 0, 80, 35));
+        addSlot(new Slot(inventory, 0, 80, 35) {
+            @Override
+            public boolean canInsert(ItemStack stack) {
+                return stack.isDamageable();
+            }
+        });
         addProperties(propertyDelegate);
         addPlayerSlots(playerInventory, 8, 84);
     }
 
     @Override
     public ItemStack quickMove(PlayerEntity player, int slot) {
-        return null;
+        ItemStack originalStack = ItemStack.EMPTY;
+        Slot originSlot = slots.get(slot);
+        if (originSlot.hasStack()) {
+            ItemStack movingStack = originSlot.getStack();
+            originalStack = movingStack.copy();
+            if (slot == 0) {
+                if (!insertItem(movingStack, 1, 37, true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else if (!insertItem(movingStack, 0, 1, false)) {
+                if (slot >= 1 && slot < 28) {
+                    if (!insertItem(movingStack, 28, 37, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                } else if (slot >= 28 && slot < 37) {
+                    if (!insertItem(movingStack, 1, 28, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                }
+            }
+
+            if (movingStack.isEmpty()) {
+                originSlot.setStack(ItemStack.EMPTY);
+            } else {
+                originSlot.markDirty();
+            }
+
+            if (movingStack.getCount() == originalStack.getCount()) {
+                return ItemStack.EMPTY;
+            }
+
+            originSlot.onTakeItem(player, movingStack);
+        }
+
+        return originalStack;
     }
 
     @Override
