33ff48a713a7d2c4c2bac5c98b363a59aba40390
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-03-03 00:41:59 +0800

    feat: add a custom totem with durability

--- src/main/java/net/ppvr/artery/items/ArteryItems.java
+++ src/main/java/net/ppvr/artery/items/ArteryItems.java
@@ -2,6 +2,8 @@ package net.ppvr.artery.items;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.DeathProtectionComponent;
 import net.minecraft.item.*;
 import net.minecraft.item.equipment.EquipmentType;
 import net.minecraft.registry.Registries;
@@ -10,6 +12,7 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.ppvr.artery.blocks.ArteryBlocks;
 
 import java.util.ArrayList;
@@ -35,6 +38,8 @@ public class ArteryItems  {
     public static final Item THROMBIUM_CHESTPLATE = register("thrombium_chestplate", settings -> new ArmorItem(ArteryArmorMaterial.THROMBIUM, EquipmentType.CHESTPLATE, settings));
     public static final Item THROMBIUM_LEGGINGS = register("thrombium_leggings", settings -> new ArmorItem(ArteryArmorMaterial.THROMBIUM, EquipmentType.LEGGINGS, settings));
     public static final Item THROMBIUM_BOOTS = register("thrombium_boots", settings -> new ArmorItem(ArteryArmorMaterial.THROMBIUM, EquipmentType.BOOTS, settings));
+
+    public static final Item TOTEM_OF_REVIVAL = register("totem_of_revival", new Item.Settings().rarity(Rarity.UNCOMMON).maxDamage(32).component(DataComponentTypes.DEATH_PROTECTION, DeathProtectionComponent.TOTEM_OF_UNDYING));
 
     public static void initialize() {
         Registry.register(Registries.ITEM_GROUP, ARTERY_ITEM_GROUP_KEY, ARTERY_ITEM_GROUP);
@@ -65,6 +70,10 @@ public class ArteryItems  {
 
     public static Item register(String id, Function<Item.Settings, Item> factory) {
         return register(keyOf(id), factory, new Item.Settings());
+    }
+
+    private static Item register(String id, Item.Settings settings) {
+        return register(keyOf(id), Item::new, settings);
     }
 
     public static Item register(RegistryKey<Item> key, Function<Item.Settings, Item> factory, Item.Settings settings) {
--- src/main/java/net/ppvr/artery/mixin/LivingEntityMixin.java
+++ src/main/java/net/ppvr/artery/mixin/LivingEntityMixin.java
@@ -1,20 +1,25 @@
 package net.ppvr.artery.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
 @Mixin(LivingEntity.class)
 abstract public class LivingEntityMixin extends Entity {
+
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
@@ -27,5 +32,14 @@ abstract public class LivingEntityMixin extends Entity {
             falloff = Math.max(1.0, distanceTo(source.getAttacker())/4.0);
         }
         attacker.artery$addUnconvertedSanguinity((float) (amount / attacker.artery$getTransfusionRate() / falloff));
+    }
+
+    @Redirect(method = "tryUseDeathProtector", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;decrement(I)V"))
+    private void tryUseDeathProtector(ItemStack itemStack, int amount, @Local Hand hand) {
+        if (itemStack.isDamageable()) {
+            itemStack.damage(1, (LivingEntity) (Object) this, LivingEntity.getSlotForHand(hand));
+        } else {
+            itemStack.decrement(amount);
+        }
     }
 }
