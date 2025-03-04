6aa926824e3d5e9e3976d5a41c24248698125cb2
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-03-04 22:31:57 +0800

    feat: add getter/setter and renderer for pressure

--- src/main/java/net/ppvr/artery/mixin/ItemStackMixin.java
+++ src/main/java/net/ppvr/artery/mixin/ItemStackMixin.java
@@ -2,15 +2,22 @@ package net.ppvr.artery.mixin;
 
 
 
+import net.minecraft.component.ComponentHolder;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.MathHelper;
+import net.ppvr.artery.hooks.ItemStackHooks;
+import net.ppvr.artery.items.ArteryDataComponentTypes;
 import net.ppvr.artery.items.ArteryItems;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,9 +27,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.function.Consumer;
 
 @Mixin(ItemStack.class)
-public class ItemStackMixin {
+public abstract class ItemStackMixin implements ComponentHolder, ItemStackHooks {
     @Unique
     private boolean shouldFlipColor;
+
+    @Shadow
+    public abstract <T> T set(ComponentType<? super T> type, @Nullable T value);
 
     @Inject(method = "appendAttributeModifierTooltip", at = @At("HEAD"))
     public void appendAttributeModifierTooltip(Consumer<Text> textConsumer, @Nullable PlayerEntity player, RegistryEntry<EntityAttribute> attribute, EntityAttributeModifier modifier, CallbackInfo ci) {
@@ -33,5 +43,21 @@ public class ItemStackMixin {
     @ModifyArg(method = "appendAttributeModifierTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/EntityAttribute;getFormatting(Z)Lnet/minecraft/util/Formatting;"))
     public boolean appendAttributeModifierTooltip(boolean addition) {
         return addition != shouldFlipColor;
+    }
+
+    public int artery$getMaxPressure() {
+        return getOrDefault(ArteryDataComponentTypes.MAX_PRESSURE, 0);
+    }
+
+    public int artery$getPressure() {
+        return MathHelper.clamp(getOrDefault(ArteryDataComponentTypes.PRESSURE, 0), 0, artery$getMaxPressure());
+    }
+
+    public void artery$setPressure(int amount) {
+        set(DataComponentTypes.DAMAGE, MathHelper.clamp(amount, 0, artery$getMaxPressure()));
+    }
+
+    public void artery$addPressure(int amount) {
+        artery$setPressure(artery$getPressure() + amount);
     }
 }
