b9228c61ecb4f9ab90edaabd12eedffe913819ec
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-03-09 23:18:55 +0800

    feat: add model and texture for pressor

--- src/main/java/net/ppvr/artery/blocks/PressorBlock.java
+++ src/main/java/net/ppvr/artery/blocks/PressorBlock.java
@@ -4,16 +4,22 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.ppvr.artery.blocks.entity.ArteryBlockEntities;
 import net.ppvr.artery.blocks.entity.OrganBlockEntity;
@@ -22,10 +28,11 @@ import org.jetbrains.annotations.Nullable;
 
 public class PressorBlock extends OrganBlock {
     public static final BooleanProperty LIT = Properties.LIT;
+    public static final EnumProperty<Direction> FACING = HorizontalFacingBlock.FACING;
 
     public PressorBlock(Settings settings) {
         super(settings);
-        setDefaultState(getDefaultState().with(LIT, false));
+        setDefaultState(getDefaultState().with(LIT, false).with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -42,6 +49,11 @@ public class PressorBlock extends OrganBlock {
     }
 
     @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
     public @Nullable <T extends BlockEntity> BlockEntityTicker<T> getTicker(World world, BlockState state, BlockEntityType<T> type) {
         return world.isClient ? null : validateTicker(type, ArteryBlockEntities.PRESSOR_BLOCK_ENTITY, PressorBlockEntity::tick);
     }
@@ -52,8 +64,18 @@ public class PressorBlock extends OrganBlock {
     }
 
     @Override
+    protected BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
-        builder.add(LIT);
+        builder.add(LIT, FACING);
     }
 }
