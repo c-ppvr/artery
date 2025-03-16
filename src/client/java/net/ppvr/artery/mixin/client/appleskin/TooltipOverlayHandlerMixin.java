dafb6ee0d904ac6bdfafd273d7fe1b4d54944a6e
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-03-17 01:06:08 +0800

    feat: render sanguinity in tooltip

---
+++ src/client/java/net/ppvr/artery/mixin/client/appleskin/TooltipOverlayHandlerMixin.java
@@ -0,0 +1,72 @@
+package net.ppvr.artery.mixin.client.appleskin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.consume.ConsumeEffect;
+import net.minecraft.util.Identifier;
+import net.ppvr.artery.hooks.TooltipOverlayHandlerHooks;
+import net.ppvr.artery.items.components.consume.GainSanguinityConsumeEffect;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import squeek.appleskin.client.TooltipOverlayHandler;
+
+import static net.ppvr.artery.Artery.MOD_ID;
+
+@Mixin(TooltipOverlayHandler.class)
+public class TooltipOverlayHandlerMixin {
+    @Mixin(TooltipOverlayHandler.FoodOverlay.class)
+    public static class FoodOverlayMixin implements TooltipOverlayHandlerHooks.FoodOverlay {
+        @Unique
+        private String sanguinityText;
+
+        @Inject(method = "<init>", at = @At("TAIL"))
+        public void init(ItemStack itemStack, FoodComponent defaultFood, FoodComponent modifiedFood, ConsumableComponent consumableComponent, PlayerEntity player, CallbackInfo ci) {
+            float totalAmount = 0;
+            for (ConsumeEffect effect : consumableComponent.onConsumeEffects()) {
+                if (effect instanceof GainSanguinityConsumeEffect(float amount)) {
+                    totalAmount += amount;
+                }
+            }
+            this.sanguinityText = "" + (int) totalAmount;
+        }
+
+        @Inject(method = "getHeight", at = @At("RETURN"), cancellable = true)
+        public void getHeight(TextRenderer textRenderer, CallbackInfoReturnable<Integer> cir) {
+            cir.setReturnValue(cir.getReturnValue() + 1 + 7);
+        }
+
+        @Inject(method = "getWidth", at = @At("RETURN"), cancellable = true)
+        public void getWidth(TextRenderer textRenderer, CallbackInfoReturnable<Integer> cir) {
+            cir.setReturnValue(Math.max(cir.getReturnValue(), textRenderer.getWidth(sanguinityText)));
+        }
+
+        @Override
+        public String artery$getSanguinityText() {
+            return sanguinityText;
+        }
+    }
+
+    @Inject(method = "onRenderTooltip", at = @At("TAIL"))
+    public void onRenderToolTip(DrawContext context, TooltipOverlayHandler.FoodOverlay foodOverlay, int toolTipX, int toolTipY, int tooltipZ, TextRenderer textRenderer, CallbackInfo ci, @Local(name = "matrixStack") MatrixStack matrixStack, @Local(name = "y") int y) {
+        int x = toolTipX;
+        y += 8;
+        context.drawTexture(RenderLayer::getGuiTextured, Identifier.of(MOD_ID, "textures/gui/singular_bar.png"), x, y + 2, 0, 0, 11, 5, 16, 16);
+        x += 11;
+        matrixStack.push();
+        matrixStack.translate(x, y, tooltipZ);
+        matrixStack.scale(0.75f, 0.75f, 0.75f);
+        context.drawTextWithShadow(textRenderer, ((TooltipOverlayHandlerHooks.FoodOverlay) foodOverlay).artery$getSanguinityText(), 2, 2, 0xFFFFCCCC);
+        matrixStack.pop();
+    }
+}
