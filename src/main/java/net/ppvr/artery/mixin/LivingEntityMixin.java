fd49360d84bb12a5df6f0fb3f627a1c0fb8af9ac
c-ppvr <85845624+c-ppvr@users.noreply.github.com>
2025-03-16 15:27:23 +0800

    refactor: optimize imports

--- src/main/java/net/ppvr/artery/mixin/LivingEntityMixin.java
+++ src/main/java/net/ppvr/artery/mixin/LivingEntityMixin.java
@@ -13,7 +13,9 @@ import net.minecraft.world.World;
 import net.ppvr.artery.items.ArteryItems;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 
